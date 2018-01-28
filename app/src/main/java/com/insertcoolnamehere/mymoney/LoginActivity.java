656b7342e19619af99d593f33e6bfa7248fa001a
Adel Hassan <sean.dashals@gmail.com>
2018-01-27 22:21:43 -0600

    added api key

--- app/src/main/java/com/insertcoolnamehere/mymoney/LoginActivity.java
+++ app/src/main/java/com/insertcoolnamehere/mymoney/LoginActivity.java
@@ -306,24 +306,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
+            String key = "fdaf93858506a13dff10d8a526650bce";
 
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            // TODO: register the new account here.
             return true;
         }
 
